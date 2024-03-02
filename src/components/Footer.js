1b30093bde3de4b82163a57023ea9ad1a1fa2f7f
sachinkasana <sachinksana@gmail.com>
2024-03-02 09:54:07 +0530

    footer changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,10 @@
 // src/components/Footer.js
 import React from 'react';
+import './footer.css'
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="sticky-footer">
       <p>&copy; 2024 CG Insurance. All rights reserved.</p>
     </footer>
   );
